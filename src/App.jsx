f187211fff276022f5bd82f8e722187e2addb32d
Carlos-93 <carlos93.bcn@hotmail.com>
2024-05-27 01:30:13 +0200

    chore: Update dependencies and configuration files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Logo from './components/Logo/Logo';
 
 export default function App() {
-  
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={
+          <main className='h-screen grid place-items-center'>
+            <Logo />
+          </main>
+        } />
+      </Routes>
+    </Router>
+  );
 }
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+    return (
+        <img
+            src="/assets/logo.png"
+            alt="Logo Tech-Play Education"
+            className="absolute top-10 right-8 w-40 xl:w-52 2xl:w-60"
+        />
+    );
+}
